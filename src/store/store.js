e01a201c6193559a8b12780159b08670b5514ba9
Aurélien <aurelien.strimelle@bstorm.be>
2023-03-07 12:00:09 +0100

    Add logger middleware 🕵️‍♀️

--- src/store/store.js
+++ src/store/store.js
@@ -2,12 +2,15 @@ import { configureStore } from '@reduxjs/toolkit';
 import peopleReducer from './reducers/people.reduce';
 import todoReducer from './slices/todo.slice';
 
+import reduxLogger from 'redux-logger';
+
 const store = configureStore({
     reducer: {
         people: peopleReducer,
         todo: todoReducer
     },
-    devTools: process.env.NODE_ENV !== 'production'
+    devTools: process.env.NODE_ENV !== 'production',
+    middleware : (getDefaultMiddleware) => [...getDefaultMiddleware(), reduxLogger]
 });
 
 export default store;
